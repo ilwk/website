f6d4f2da1ba60c69cd41b478c0ea629e52f4402a
luwj <luwj@greatld.com>
2023-07-07 10:11:15 +0800

    fix: fix build

--- components/MDXContent.tsx
+++ components/MDXContent.tsx
@@ -4,6 +4,7 @@ import HTMLReactParser from 'html-react-parser';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 
 export const MDXContent = async ({ post }: { post: Post }) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const Conetnt = useMDXComponent(post.body.code);
   const hightlight = await highlighterPromise;
   return (
