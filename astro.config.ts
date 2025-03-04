2e819b4679c10310b4e89d481cbe51c4a23473a1
luwj <luwj.dev@gmail.com>
2025-03-04 11:28:05 +0800

    refactor: 移除astrojs/tailwind，添加tailwindcss/vite，并更新相关依赖版本

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/consts";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.WEBSITE,
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
